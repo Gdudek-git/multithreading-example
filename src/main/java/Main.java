fea09d02206b06a54a12fee1770b208f76711968
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-07-29 13:55:26 +0200

    add scene

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/MapView.fxml"));
+        primaryStage.setTitle("MultithreadingExample");
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
 
