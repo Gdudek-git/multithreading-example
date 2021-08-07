398668fb9fa2c8cbb1dfe6320e423f96961262ea
Grzegorz Dudek <74261917+MeZziK@users.noreply.github.com>
2021-08-07 16:46:50 +0200

    change runnable to task

--- src/main/java/controller/MapViewController.java
+++ src/main/java/controller/MapViewController.java
@@ -2,31 +2,40 @@ package controller;
 
 
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import model.display.CarDisplayManager;
 import model.drivers.CarDriver;
 import model.drivers.TrainDriver;
+import model.railroad_tracks.CrossingKeeper;
 import model.spawner.CarSpawner;
 import model.spawner.TrainSpawner;
 import model.vechicle.Car;
 import model.vechicle.Train;
 import utils.AddVehicleImageToCorrectPane;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class MapViewController {
 
     @FXML
-    private AnchorPane rootPane;
-    @FXML
     private AnchorPane bottomPane;
     @FXML
     private AnchorPane topPane;
-
+    @FXML
+    private ImageView barrierLights;
+    @FXML
+    private ImageView bottomBarrier;
+    @FXML
+    private ImageView topBarrier;
 
 
     @FXML
-    public void initialize() {
+    public void initialize()
+    {
         startSpawning();
     }
 
@@ -38,57 +47,78 @@ public class MapViewController {
 
         Runnable cars = () -> spawnCars(carSpawner);
         Thread car = new Thread(cars);
+        car.setDaemon(true);
         car.start();
 
-        Runnable trains = () -> spawnTrain(trainSpawner);
-        Thread train = new Thread(trains);
-        train.start();
+        spawnTrain(trainSpawner);
     }
 
     private void spawnCars(CarSpawner carSpawner)
     {
         while(true)
         {
-            Runnable r = () ->
+            Task<Void> task = new Task<>()
             {
-                Car car = carSpawner.spawnCar();
-                if(car!=null)
+                @Override
+                protected Void call()
                 {
-                    CarDisplayManager carDisplayManager = new CarDisplayManager(bottomPane,topPane);
-                    CarDriver carDriver = new CarDriver(car,carDisplayManager);
-                    carSpawner.increaseAmountOfSpawnedCars();
-                    AddVehicleImageToCorrectPane.addVehicleImageToCorrectPane(car,bottomPane,topPane);
-                    carDriver.drive();
-                    carSpawner.decreaseAmountOfSpawnedCars();
+                    Car car = carSpawner.spawnCar();
+                    if (car != null)
+                    {
+                        CarDisplayManager carDisplayManager = new CarDisplayManager(bottomPane, topPane);
+                        CarDriver carDriver = new CarDriver(car, carDisplayManager);
+                        carSpawner.increaseAmountOfSpawnedCars();
+                        AddVehicleImageToCorrectPane.addVehicleImageToCorrectPane(car, bottomPane, topPane);
+                        carDriver.drive();
+                        carSpawner.decreaseAmountOfSpawnedCars();
+                    }
+                    return null;
                 }
             };
-            Thread t = new Thread(r);
+            Thread t = new Thread(task);
+            t.setDaemon(true);
             t.start();
-            try
-            {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            waitBeforeSpawningAnotherCar();
+        }
+    }
+
+    private void waitBeforeSpawningAnotherCar()
+    {
+        try
+        {
+            Thread.sleep(ThreadLocalRandom.current().nextInt(3000,5000));
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
         }
     }
 
     private void spawnTrain(TrainSpawner trainSpawner)
     {
-        while(true) {
-            Train train = trainSpawner.spawnTrain();
-            TrainDriver trainDriver = new TrainDriver(train);
-            Platform.runLater(()->topPane.getChildren().add(train.getImageView()));
-            trainDriver.drive();
-            try
+        CrossingKeeper crossingKeeper = new CrossingKeeper(topBarrier,bottomBarrier,barrierLights);
+        Task<Void> trains = new Task<>()
+        {
+            @Override
+            protected Void call()
             {
-                Thread.sleep(30000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                while(true)
+                {
+                    Train train = trainSpawner.spawnTrain();
+                    TrainDriver trainDriver = new TrainDriver(train,crossingKeeper);
+                    Platform.runLater(()->topPane.getChildren().add(train.getImageView()));
+                    trainDriver.drive();
+                    try
+                    {
+                        Thread.sleep(23000);
+                    } catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
-
+        };
+        Thread train = new Thread(trains);
+        train.setDaemon(true);
+        train.start();
     }
-
-
 }
